078cf97496ee61b3d47e56738a2b655adde03f4d
Karthik gurram <karthik3583@gmail.com>
2024-08-26 04:58:13 +0530

    Update prober.py
    Key Fixes and Improvements:
    Error Handling: Improved error handling, particularly with the EasySNMPTimeoutError, to ensure the script continues running smoothly in case of a timeout.
    Sampling Frequency Calculation: Ensured that the sampling frequency is accurately calculated and used in the loop and sleep logic.
    Loop and Control Flow: Corrected the loop control to ensure the loop iterates correctly and samples are processed accurately.
    Output Handling: Improved output formatting to handle cases where OID values are missing or instances are not available.
    Optimized Sleep Logic: Refined the sleep logic to make it more efficient and avoid busy waiting.
    Variable Cleanup: Cleaned up variables and ensured they are reset as needed, especially the out1 list.

--- A2/prober.py
+++ A2/prober.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python
-# > libraries import
-from easysnmp import *
+
+from easysnmp import Session, EasySNMPTimeoutError
 import sys
 import time
 import math
 
-# extract all the info from the command line parameters here
+# Extract command line arguments
 ip_address = sys.argv[1].split(':')[0]  # IP address of SNMP agent
-remote_port = sys.argv[1].split(':')[1] # > port of the SNMP agent
-community = sys.argv[1].split(':')[2] # > community of the SNMP agent
+remote_port = sys.argv[1].split(':')[1]  # Port of the SNMP agent
+community = sys.argv[1].split(':')[2]  # Community of the SNMP agent
 
-sampling_freq = float(sys.argv[2])  # handle between 10 and 0.1 Hz.
+sampling_freq = float(sys.argv[2])  # Sampling frequency (Hz)
 sampling_int = 1 / sampling_freq
-samples = int(sys.argv[3])  # Samples (N) is the number of successful samples the solution should do before terminating
-# should be greater than or equal 2
+samples = int(sys.argv[3])  # Number of samples to collect
 
-# > get a list of SNMP object from the command line
+# List of OIDs to query
 list_oids = sys.argv[4:]
-# > add this SNMP object to the command line argument
-# > this means the time (in hundredths of a second) since the network management portion of
-# > the system was last re-initialized. 
-# > reference: http://www.alvestrand.no/objectid/1.3.6.1.2.1.1.3.html
-list_oids.insert(0, '1.3.6.1.2.1.1.3.0')
-# Create an SNMP session to be used for all our requests
-session = Session(hostname=ip_address, remote_port=remote_port, community='public', version=2,timeout=5, retries=1 )
+list_oids.insert(0, '1.3.6.1.2.1.1.3.0')  # SNMP uptime OID
+
+# Create SNMP session
+session = Session(hostname=ip_address, remote_port=remote_port, community=community, version=2, timeout=5, retries=1)
 time_ticks11 = 0.0
 timeout = False
 i = 1
@@ -33,144 +29,74 @@ old = []
 out1 = []
 time_start = time.time()
 
-# > this procedure sleeps n cycles according to the variable sampling_int 
+# Sleep until the next cycle based on the sampling interval
 def sleep_until_next_cycle():
-    # get now time
     now_time = time.time()
-
-    # compute how much time from the beginning
     time_until_now = now_time - time_start
-
-    # compute the cycles that have passed
     time_cycles_till_now = math.floor(float(time_until_now) / float(sampling_int))
-
-    # compute just the previous cycle point in time
     previous_cycle_time = time_start + time_cycles_till_now * sampling_int
-
-    # set a counter for the next cycle (we are not sure it is the next as time runs)
     time_cycles_from_now_on = 1
 
-    # loop to reach to a sleep
     while True:
-        # compute next possible cycle (it is the previous one plus one, plus two, .....)
         next_cycle = previous_cycle_time + time_cycles_from_now_on * sampling_int
-
-        # compute the time we have to sleep
         sleep_interval = next_cycle - time.time()
 
-        # if it is zero (we are very lucky!!!) and directly return
-        if sleep_interval == 0.0:
-            return
-        # if it is more than zero first sleep then return
-        elif sleep_interval > 0.0:
-            # print 'timeout time_to_sleep = ', time_to_sleep and then return
+        if sleep_interval <= 0:
+            time_cycles_from_now_on += 1
+        else:
             time.sleep(sleep_interval)
             return
 
-        # if less than zero then increment and recalculate
-        time_cycles_from_now_on = time_cycles_from_now_on + 1
-
-# > Main function
-while i<= samples:
-
-    # > start processsing all the samples
+# Main loop
+while i <= samples:
     time_stamp_x = int(time.time())
     try:
-        # send a snmp request at a required sampling frequency
-        # i.e send request every 1/sampling_freq time interval
         oid_replies = session.get(list_oids)
-        # > new is the variable that will receive the request
         new = oid_replies
-        # > time_ticks22 is the variable that will get the time since initialization
         time_ticks22 = float(new[0].value)
-        # > set timeout to true if there is an error
     except EasySNMPTimeoutError:
         timeout = True
-        pass
-    time_stamp_y = int(time.time())
+        continue
 
+    time_stamp_y = int(time.time())
     response_time = time_stamp_y - time_stamp_x
-    #try:
-        
-    #    time_ticks22 = float(session.get(['1.3.6.1.2.1.1.3.0'])[0].value)  
-    #except:
-    #    time_ticks22 = 0.0
-    #    pass
-    # lets do some stuff here
-    #  print data
+
     if len(new) == len(old):
-        # > get time difference between sample ticks
         time_ticks = time_ticks22 - time_ticks11
-        # > if sample ticks is negative...
         if time_ticks < 0:
-            print ("The Device has been Rebooted")
+            print("The Device has been Rebooted")
         else:
-            # > calculate difference between the two samples
-            if sampling_freq > 1:
-                timediff = time_stamp_x - t4
-            if sampling_freq <= 1:
-                timediff1 = time_stamp_x - t4
-                if timediff1 != 0:
-                    timediff = timediff1
-                else:
-                    timediff = int(sampling_int)
-            # > run a for each SNMP ID stored in array list_oids[]
+            timediff = max(time_stamp_x - t4, int(sampling_int))
+
             for x in range(1, len(list_oids)):
-                # > if the object exists (different than NOSUCHINSTANCE result)...
                 if new[x].value != "NOSUCHINSTANCE" and old[x].value != "NOSUCHINSTANCE":
-                    # > assign new and old timestamp to variable nvalue and ovalue
                     nvalue = int(new[x].value)
                     ovalue = int(old[x].value)
-                    # > if new timestamp is greater or equal than old...
-                    if  nvalue>= ovalue:
-                        # > if time difference is 0
-                        if timediff == 0: 
-                            # > output will be 0
-                            out = 0
-                        else:
-                            # > output will be new value minus old value divided by time difference
-                            out = ( nvalue- ovalue) / timediff
-                            # > append to array out1[]
-                        out1.append(out)
-                    # > if the new timestamp is less than the old...
-                    if nvalue < ovalue:
-                        # > get SNMPv2 COUNTER64 object if it exists
-                        if new[x].snmp_type == "COUNTER64":
-                            out = ((2 ** 64 + nvalue) - ovalue) / timediff
-                            # > append to the array out1[]
-                            out1.append(out)
-                        # > get SNMP COUNTER object if it exists
-                        if new[x].snmp_type == "COUNTER":
-                            out = ((2 ** 32 + nvalue) - ovalue) / timediff
-                            # > append to the array out1[]
-                            out1.append(out)
+                    if nvalue >= ovalue:
+                        out = (nvalue - ovalue) / timediff if timediff != 0 else 0
+                    elif new[x].snmp_type == "COUNTER64":
+                        out = ((2 ** 64 + nvalue) - ovalue) / timediff
+                    elif new[x].snmp_type == "COUNTER":
+                        out = ((2 ** 32 + nvalue) - ovalue) / timediff
+                    else:
+                        out = "N/A"
+                    out1.append(out)
                 else:
-                    # > print timestamp if new value is equal to NOSUCHINSTANCE
-                    print time_stamp_x, "|"
-        # > increment count to get next sample
-        i = i + 1
-        # > if length of array out1[] is greater than zero
-        if len(out1) != 0:
-            # > print the array out1[]
-            sar = [str(get) for get in out1]
-            print time_stamp_x, '|', ("| ".join(sar))
-    # > assing requests on the array new[] to the array old[] 
+                    print(time_stamp_x, "| NOSUCHINSTANCE")
+                    continue
+
+            if len(out1) != 0:
+                sar = [str(get) for get in out1]
+                print(time_stamp_x, '|', " | ".join(sar))
+
     old = new[:]
-    # > assign end of loop time to t4
     t4 = time_stamp_x
-    # > clean array out1[]
-    del out1[:]
+    out1.clear()
 
     if timeout:
-        timeout = True
-    # > call routine to sleep
+        print("Timeout occurred")
+        timeout = False
+
     sleep_until_next_cycle()
-
-    # > save time since last initialization for comparison at the beginning of the loop
     time_ticks11 = time_ticks22
-    # > this command marks the end of the while loop
-    pass
-
-
-
-
+    i += 1
